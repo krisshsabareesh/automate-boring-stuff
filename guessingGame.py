3d4f76ee82827abc61347318ac9fff7e9368b2e1
Sabareesh Krishnan <krisshsabareesh@gmail.com>
2016-08-17 21:41:27 +0530

    Guessing game

--- guessingGame.py
+++ guessingGame.py
@@ -4,11 +4,16 @@ while guess not in ('heads', 'tails'):
     print('Guess the coin toss! Enter heads or tails:')
     guess = input()
 toss = random.randint(0, 1) # 0 is tails, 1 is heads
+
+if guess == 'heads':
+  guess = 0
+else:
+  guess = 1
 if toss == guess:
     print('You got it!')
 else:
     print('Nope! Guess again!')
-    guesss = input()
+    guess = input()
     if toss == guess:
        print('You got it!')
     else:
